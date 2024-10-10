9b4cae23d8284220b96e5f8036a5d2c5c6b757c8
seo <rtaa4234@naver.com>
2024-10-10 18:04:51 +0900

    random에 대한 이해

---
+++ exam10/hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    static  void hello(){
+        System.out.println("안녕하세요.");
+    }
+    public static  void main(String[] args) {
+        hello();
+        hello();
+        hello();
+
+    }
+}
